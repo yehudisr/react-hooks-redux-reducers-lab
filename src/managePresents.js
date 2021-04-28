4e396e8e7887a29b240903f38099e4f354635dd3
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-27 21:24:40 -0400

    commit tree 63bff9509fddabb62f2b0dc6db9dca238df27a37

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,10 @@
 export function managePresents(state, action) {
+
+   switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
   // your code here
 }
